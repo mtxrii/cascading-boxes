dd9f50e49171cb1c5199ead6f15c3fa0fd06c3df
Enrique <edavalos@ucsc.edu>
2020-11-15 04:56:58 -0800

    Added scheme

--- box.scm
+++ box.scm
@@ -1,24 +1,25 @@
+#lang racket
+
 (define x 4)
 
 (display "+")
-(define lp (i)
-  (cond (< i x)
-    (
-      (display "-+\n")
-      (define lpi (j)
-        (cond (< j i)
-          (display "  "))
-        (lpi (+ 1 j)))
-      (lpi 0)
-      
-      (display "| |\n")
-      (define lpii (ji)
-        (cond (< ji i)
-          (display "  "))
-        (lpii (+ 1 ji)))
-      (lpii 0)
-      
-      (display "+-+")
-    )
-  (lp (+ 1 i))))
+(define (lp i)
+  (when (< i x)
+    
+    (display "-+\n")
+    (define (lpi j)
+      (when (< j i)
+        (display "  ")
+        (lpi (+ 1 j))))
+    (lpi 0)
+
+    (display "| |\n")
+    (define (lpii ji)
+      (when (< ji i)
+        (display "  ")
+        (lpii (+ 1 ji))))
+    (lpii 0)
+
+    (display "+-+")
+    (lp (+ 1 i))))
 (lp 0)
